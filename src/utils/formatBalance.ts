cc4feacf0f2dfd90b8c0690e26a1ffd1bbcf7c2d
Ignacio Freijomil <90807212+ArtemisProtocol@users.noreply.github.com>
2021-11-10 12:15:45 -0300

    revert auto-rvrs

--- src/utils/formatBalance.ts
+++ src/utils/formatBalance.ts
@@ -8,15 +8,3 @@ export const getBalanceNumber = (balance: BigNumber, decimals = 18) => {
 export const getFullDisplayBalance = (balance: BigNumber, decimals = 18) => {
   return balance.dividedBy(new BigNumber(10).pow(decimals)).toFixed()
 }
-
-export const getDecimalAmount = (amount: BigNumber, decimals = 18) => {
-  return new BigNumber(amount).times(new BigNumber(10).pow(decimals));
-}
-
-export const formatNumber = (number: number, minPrecision = 2, maxPrecision = 2) => {
-  const options = {
-    minimumFractionDigits: minPrecision,
-    maximumFractionDigits: maxPrecision
-  };
-  return number.toLocaleString(undefined, options);
-};
--- src/views/Pools/types.ts
+++
@@ -1,9 +0,0 @@
-import BigNumber from 'bignumber.js';
-
-export interface VaultUser {
-  shares: BigNumber;
-  crystalAtLastUserAction: BigNumber;
-  lastDepositedTime: string;
-  lastUserActionTime: string;
-  stakingTokenBalance: BigNumber;
-}
